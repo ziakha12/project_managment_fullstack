dc7fbc2d8ff5aae8e9ba7fa8b846f9700c52507b
ziakha12 <kzzia06@gmail.com>
2026-09-18 17:55:33 +0500

    test push

--- models/OrganizationModel.ts
+++ models/OrganizationModel.ts
@@ -1,102 +1,91 @@
-import mongoose, {Schema, Document} from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
 import { userInterface } from "./UserModel";
+import { planInterface } from "./PlanModel";
 
 export interface organiztaionInterface extends Document {
-    name : string,
-    owner : userInterface | string,
-    slug : string,
-    logo : string,
-    organizationStatus : "active" | "inactive" | "suspended",
-    plan : {
-        type : string,
-        seatsAllowed : number,
-        seatsUsed : number,
-        planStatus : "active" | "inactive" | "suspended",
-        trailEndDate : Date,
-        isActive : boolean
-    },
-    setting : {
-        timezone : string,
-        currency : string,
-        aiFeatureEnabled : boolean,
-        workingDays : string[],
+    name: string,
+    owner: userInterface | string,
+    slug: string,
+    logo: string,
+    billing: {
+        plan: string | planInterface,
+        seatsUsed: number,
+        planStatus: string,
+        isPlanActive : boolean
+    }
+    organizationStatus: "active" | "inactive" | "suspended",
+    setting: {
+        timezone: string,
+        currency: string,
+        aiFeatureEnabled: boolean,
+        workingDays: string[],
     }
 }
 
 const organizationSchema = new Schema<organiztaionInterface>(
     {
-        name : {
-            type : String,
-            required : true,
-            
+        name: {
+            type: String,
+            required: true,
         },
-        slug : {
-            type : String,
-            required : true, 
+        slug: {
+            type: String,
+            required: true,
         },
-        logo : {
-            type : String,
+        logo: {
+            type: String,
         },
-        owner : {
-            type : mongoose.Schema.Types.ObjectId,
-            ref : "User", 
-            required : true
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
         },
-        organizationStatus : {
-            type : String,
-            required : true,
-            deafault : "active",
-            enum : ["active", "inactive", "suspended"]
+        organizationStatus: {
+            type: String,
+            required: true,
+            deafault: "active",
+            enum: ["active", "inactive", "suspended"]
         },
-        plan : {
-            type : {
-                type : String,
-                enum : ["free", "pro", "enterprise"]
-            },
-            seatsAllowed : {
-                type : Number,
-                default : 3
+        billing : {
+            plan: {
+                type: Schema.Types.ObjectId,
+                ref: "Plan",
+                default: 'free'
             },
             seatsUsed : {
                 type : Number,
-                default : 0
+            },
+            isPlanActive : {
+                type : Boolean,
+                default : true
             },
             planStatus : {
                 type : String,
-                enum : ["active", "inactive", "suspended"],
-                default : "active"
+                default : 'active',
             },
-            trailEndDate : {
-                type : Date
-            },
-            isActive : {
-                type : Boolean,
-                default : true
-            }
         },
-        setting : {
-            timezone : {
-                type : String,
-                default : "UTC"
+        setting: {
+            timezone: {
+                type: String,
+                default: "UTC"
             },
-            currency : {
-                type : String,
-                default : "USD"
+            currency: {
+                type: String,
+                default: "USD"
             },
-            aiFeatureEnabled : {
-                type : Boolean,
-                default : false
+            aiFeatureEnabled: {
+                type: Boolean,
+                default: false
             },
-            workingDays : {
-                type : [String],
-                default : ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+            workingDays: {
+                type: [String],
+                default: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
             }
         }
     },
     {
-        timestamps : true
+        timestamps: true
     }
 )
 
 export const Organization = mongoose.models.organizations || mongoose.model<organiztaionInterface>('Organization', organizationSchema)
-
